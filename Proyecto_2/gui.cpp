3d3ecf02d19c6fe69122695521ef4758aa2c4def
CarlosPR151 <penasrecinos@gmail.com>
2018-11-25 20:50:32 -0600

    final

--- Proyecto_2/gui.cpp
+++ Proyecto_2/gui.cpp
@@ -2,6 +2,8 @@
  #include <iostream>
  #include <thread>
  #include <chrono>
+#include <fstream>
+
 typedef std::chrono::high_resolution_clock Clock;
 
  extern "C" int calcularProbabilidad(int unidadDeTiempo,int probabilidad,int correccion,int desecho);
@@ -14,15 +16,15 @@ class App : public Gtk::Window
             label1.set_text("Unidad de tiempo (segundos)");
             correccionLabel.set_text("Tiempo de corrección (segundos)");
             desechoLabel.set_text("Tiempo de desecho (segundos)");
-            etapa1.set_text("Etapa 1");
-            etapa2.set_text("Etapa 2");
-            etapa3.set_text("Etapa 3");
-            etapa4.set_text("Etapa 4");
-            etapa5.set_text("Etapa 5");
+            etapa1.set_text("Coccion");
+            etapa2.set_text("Limpieza");
+            etapa3.set_text("Enaltado");
+            etapa4.set_text("Esterilizacion");
+            etapa5.set_text("Empaque");
             porcentajes.set_text("Porcentajes de error por etapa");
 
             set_default_size(1000, 800);
-            set_title("Pipeline");
+            set_title("Fabrica de atun");
             set_position(Gtk::WIN_POS_CENTER);
             set_border_width(10);
 
@@ -151,7 +153,7 @@ class App : public Gtk::Window
 
     //private:
         void onClick(){
-            startProgram.override_background_color(Gdk::RGBA("red"));
+            std::this_thread::sleep_for(std::chrono::seconds(1));
             std::thread t(startProgramOnClick,this);
             t.detach();
         }
@@ -171,84 +173,269 @@ class App : public Gtk::Window
                 std::cout << "ENTRADA INVALIDA" << std::endl;
                 return;
             }
-            window->startLine(unidad,porcentaje1,porcentaje2,porcentaje3,porcentaje4,porcentaje5,correccion,desecho);
+            window->start(unidad,porcentaje1,porcentaje2,porcentaje3,porcentaje4,porcentaje5,correccion,desecho);
         }
 
         int prob(int unidad,int porcentaje,int correccion,int desecho){
             return calcularProbabilidad(unidad,porcentaje,correccion,desecho);
         }
 
-        void startLine(int unidad,int porcentaje1,int porcentaje2,int porcentaje3,int porcentaje4,int porcentaje5,int correccion,int desecho){
-                int error=0;
-                for(int i=0;i<5;i++){
-                    auto t1 = Clock::now();
-                    switch(i){
+        void etapa5Run(int unidad,int porcentaje1,int porcentaje2,int porcentaje3,int porcentaje4,int porcentaje5,int correccion,int desecho){
+            int error=0;
+            auto t1 = Clock::now();
+            switch(cola5){
+                case 0:
+                    cola5++;
+                    etapa5_1.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje5,correccion,desecho);
+                    etapa5_1.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola5--;
+                    break;
+                case 1:
+                    cola5++;
+                    etapa5_2.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje5,correccion,desecho);
+                    etapa5_2.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola5--;
+                    break;
+                case 2:
+                    cola5++;
+                    etapa5_3.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje5,correccion,desecho);
+                    etapa5_3.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola5--;
+                    break;
+            }
+            switch(error){
                         case 0:
-                            etapa1_1.override_background_color(Gdk::RGBA("red"));
-                            etapa2_1.override_background_color(Gdk::RGBA("white"));
-                            etapa3_1.override_background_color(Gdk::RGBA("white"));
-                            etapa4_1.override_background_color(Gdk::RGBA("white"));
-                            etapa5_1.override_background_color(Gdk::RGBA("white"));
-                            this->show_all_children();
-                            error = prob(unidad,porcentaje1,correccion,desecho);
+                            histograma << "Etapa 5:No hay error" << std::endl;
                             break;
                         case 1:
-                            etapa1_1.override_background_color(Gdk::RGBA("white"));
-                            etapa2_1.override_background_color(Gdk::RGBA("red"));
-                            etapa3_1.override_background_color(Gdk::RGBA("white"));
-                            etapa4_1.override_background_color(Gdk::RGBA("white"));
-                            etapa5_1.override_background_color(Gdk::RGBA("white"));
-                            this->show_all_children();
-                            error = prob(unidad,porcentaje2,correccion,desecho);
+                            histograma << "Etapa 5:Correccion" << std::endl;
                             break;
                         case 2:
-                            etapa1_1.override_background_color(Gdk::RGBA("white"));
-                            etapa2_1.override_background_color(Gdk::RGBA("white"));
-                            etapa3_1.override_background_color(Gdk::RGBA("red"));
-                            etapa4_1.override_background_color(Gdk::RGBA("white"));
-                            etapa5_1.override_background_color(Gdk::RGBA("white"));
-                            this->show_all_children();
-                            error = prob(unidad,porcentaje3,correccion,desecho);
-                            break;
-                        case 3:
-                            etapa1_1.override_background_color(Gdk::RGBA("white"));
-                            etapa2_1.override_background_color(Gdk::RGBA("white"));
-                            etapa3_1.override_background_color(Gdk::RGBA("white"));
-                            etapa4_1.override_background_color(Gdk::RGBA("red"));
-                            etapa5_1.override_background_color(Gdk::RGBA("white"));
-                            this->show_all_children();
-                            error = prob(unidad,porcentaje4,correccion,desecho);
-                            break;
-                        case 4:
-                            etapa1_1.override_background_color(Gdk::RGBA("white"));
-                            etapa2_1.override_background_color(Gdk::RGBA("white"));
-                            etapa3_1.override_background_color(Gdk::RGBA("white"));
-                            etapa4_1.override_background_color(Gdk::RGBA("white"));
-                            etapa5_1.override_background_color(Gdk::RGBA("red"));
-                            this->show_all_children();
-                            error = prob(unidad,porcentaje5,correccion,desecho);
-                            break;
-                    }
-                    std::cout << i << std::endl;
-                    switch(error){
-                        case 0:
-                            std::cout << "No hay error" << std::endl;
-                            break;
-                        case 1:
-                            std::cout << "Correccion" << std::endl;
-                            break;
-                        case 2:
-                            std::cout << "Desecho" << std::endl;
+                            histograma << "Etapa 5:Desecho" << std::endl;
                             break;
                     }
                     auto t2 = Clock::now();
-                    std::cout << "Tiempo: "  << (int)(std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() / 1e+9) << std::endl;
-                    std::cout << std::endl;
+            histograma << "Tiempo: " << (int)(std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() / 1e+9) << std::endl;
+        }
+
+        void etapa4Run(int unidad,int porcentaje1,int porcentaje2,int porcentaje3,int porcentaje4,int porcentaje5,int correccion,int desecho){
+            int error=0;
+            auto t1 = Clock::now();
+            switch(cola4){
+                case 0:
+                    cola4++;
+                    etapa4_1.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje4,correccion,desecho);
+                    etapa4_1.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola4--;
+                    break;
+                case 1:
+                    cola4++;
+                    etapa4_2.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje4,correccion,desecho);
+                    etapa4_2.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola4--;
+                    break;
+                case 2:
+                    cola4++;
+                    etapa4_3.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje4,correccion,desecho);
+                    etapa4_3.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola4--;
+                    break;
+            }
+            switch(error){
+                        case 0:
+                            histograma << "Etapa 4:No hay error" << std::endl;
+                            break;
+                        case 1:
+                            histograma << "Etapa 4:Correccion" << std::endl;
+                            break;
+                        case 2:
+                            histograma << "Etapa 4:Desecho" << std::endl;
+                            break;
+                    }
+                    auto t2 = Clock::now();
+            histograma << "Tiempo: " << (int)(std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() / 1e+9) << std::endl;
+            etapa5Run(unidad,porcentaje1,porcentaje2,porcentaje3,porcentaje4,porcentaje5,correccion,desecho);
+        }
+
+        void etapa3Run(int unidad,int porcentaje1,int porcentaje2,int porcentaje3,int porcentaje4,int porcentaje5,int correccion,int desecho){
+            int error=0;
+            auto t1 = Clock::now();
+            switch(cola3){
+                case 0:
+                    cola3++;
+                    etapa3_1.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje3,correccion,desecho);
+                    etapa3_1.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola3--;
+                    break;
+                case 1:
+                    cola3++;
+                    etapa3_2.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje3,correccion,desecho);
+                    etapa3_2.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola3--;
+                    break;
+                case 2:
+                    cola3++;
+                    etapa3_3.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje3,correccion,desecho);
+                    etapa3_3.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola3--;
+                    break;
+            }
+            switch(error){
+                        case 0:
+                            histograma << "Etapa 3:No hay error" << std::endl;
+                            break;
+                        case 1:
+                            histograma << "Etapa 3:Correccion" << std::endl;
+                            break;
+                        case 2:
+                            histograma << "Etapa 3:Desecho" << std::endl;
+                            break;
+                    }
+                    auto t2 = Clock::now();
+            histograma << "Tiempo: " << (int)(std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() / 1e+9) << std::endl;
+            etapa4Run(unidad,porcentaje1,porcentaje2,porcentaje3,porcentaje4,porcentaje5,correccion,desecho);
+        }
+
+        void etapa2Run(int unidad,int porcentaje1,int porcentaje2,int porcentaje3,int porcentaje4,int porcentaje5,int correccion,int desecho){
+            int error=0;
+            auto t1 = Clock::now();
+            switch(cola2){
+                case 0:
+                    cola2++;
+                    etapa2_1.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje2,correccion,desecho);
+                    etapa2_1.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola2--;
+                    break;
+                case 1:
+                    cola2++;
+                    etapa2_2.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje2,correccion,desecho);
+                    etapa2_2.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola2--;
+                    break;
+                case 2:
+                    cola2++;
+                    etapa2_3.override_background_color(Gdk::RGBA("red"));
+                    show_all_children();
+                    error = prob(unidad,porcentaje2,correccion,desecho);
+                    etapa2_3.override_background_color(Gdk::RGBA("white"));
+                    show_all_children();
+                    cola2--;
+                    break;
+            }
+            switch(error){
+                        case 0:
+                            histograma << "Etapa 2:No hay error" << std::endl;
+                            break;
+                        case 1:
+                            histograma << "Etapa 2:Correccion" << std::endl;
+                            break;
+                        case 2:
+                            histograma << "Etapa 2:Desecho" << std::endl;
+                            break;
+                    }
+                    auto t2 = Clock::now();
+            histograma << "Tiempo: " << (int)(std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() / 1e+9) << std::endl;
+            etapa3Run(unidad,porcentaje1,porcentaje2,porcentaje3,porcentaje4,porcentaje5,correccion,desecho);
+        }
+
+        void static etapa1Run(App *window,int unidad,int porcentaje1,int porcentaje2,int porcentaje3,int porcentaje4,int porcentaje5,int correccion,int desecho){
+            int error=0;
+            auto t1 = Clock::now();
+            switch(window->cola1){
+                case 0:
+                    window->cola1++;
+                    window->etapa1_1.override_background_color(Gdk::RGBA("red"));
+                    window->show_all_children();
+                    error = window->prob(unidad,porcentaje1,correccion,desecho);
+                    window->etapa1_1.override_background_color(Gdk::RGBA("white"));
+                    window->show_all_children();
+                    window->cola1--;
+                    break;
+                case 1:
+                    window->cola1++;
+                    window->etapa1_2.override_background_color(Gdk::RGBA("red"));
+                    window->show_all_children();
+                    error = window->prob(unidad,porcentaje1,correccion,desecho);
+                    window->etapa1_2.override_background_color(Gdk::RGBA("white"));
+                    window->show_all_children();
+                    window->cola1--;
+                    break;
+                case 2:
+                    window->cola1++;
+                    window->etapa1_3.override_background_color(Gdk::RGBA("red"));
+                    window->show_all_children();
+                    error = window->prob(unidad,porcentaje1,correccion,desecho);
+                    window->etapa1_3.override_background_color(Gdk::RGBA("white"));
+                    window->show_all_children();
+                    window->cola1--;
+                    break;
+            }
+            switch(error){
+                        case 0:
+                            window->histograma << "Etapa 1:No hay error" << std::endl;
+                            break;
+                        case 1:
+                            window->histograma << "Etapa 1:Correccion" << std::endl;
+                            break;
+                        case 2:
+                            window->histograma << "Etapa 1:Desecho" << std::endl;
+                            break;
+                    }
+                    auto t2 = Clock::now();
+            window->histograma << "Tiempo: " << (int)(std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() / 1e+9) << std::endl;
+            window->etapa2Run(unidad,porcentaje1,porcentaje2,porcentaje3,porcentaje4,porcentaje5,correccion,desecho);
+        }
+
+        void start(int unidad,int porcentaje1,int porcentaje2,int porcentaje3,int porcentaje4,int porcentaje5,int correccion,int desecho){
+            histograma.open("histograma.txt",std::ios::app);
+            while(true){
+                if(cola1 <= 2){
+                    std::thread start(etapa1Run,this,unidad,porcentaje1,porcentaje2,porcentaje3,porcentaje4,porcentaje5,correccion,desecho);
+                    start.detach();
+                    std::cout << cola1 << "," << cola2 << "," << cola3 << std::endl;
                 }
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }
+            histograma.close();
         }
 
 
         // Elementos de la ventana
+
+        std::ofstream histograma;
 
         Gtk::Grid grid;
 
